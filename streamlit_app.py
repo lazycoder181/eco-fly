abf52f98a0975d16d37df15d4d9cec72b8ed2a31
Aditya Rane <adityasatyaprakash.rane@jcu.edu.au>
2022-10-04 12:57:59 +0800

    deployment update css

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ def local_css(file_name):
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
-local_css("C:/Users/adrane/PycharmProjects/Snowflake_Streamlit/style/style.css")
+local_css("C:/Users/adrane/PycharmProjects/Snowflake_Streamlit/style.css")
 
 # ---- LOAD ASSETS ----
 lottie_emission = load_lottieurl("https://assets1.lottiefiles.com/private_files/lf30_dy2xdidy.json")
