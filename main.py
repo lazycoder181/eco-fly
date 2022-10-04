57237107e05de551ff6a1cd226e506be90b8d007
Aditya Rane <adityasatyaprakash.rane@jcu.edu.au>
2022-10-04 22:43:47 +0800

    main

--- main.py
+++ main.py
@@ -21,3 +21,26 @@ import streamlit.components.v1 as components
 # embed streamlit docs in a streamlit app
 # components.iframe("https://www.baidu.com")
 
+# pr_1 = pd.read_csv("pr_1.csv")
+# print(pr_1)
+#
+# pop_routes_origin_dict = {"NOC": 'Ireland West Knock Airport, Ireland',
+#                           "TGD": 'Podgorica Airport, Montenegro',
+#                           "KWA": 'Bucholz Army Air Field, Marshall Islands',
+#                           "CGH": 'Congonhas Airport, Brazil', "CKH": 'Chokurdakh Airport, Russia',
+#                           "HDF": 'Heringsdorf Airport, Germany', "OND": 'Ondangwa Airport, Namibia',
+#                           "XMH": 'Manihi Airport', "LUA": 'Lukla Airport, Nepal',
+#                           "AKX": 'Aktobe Airport, Kazakhstan'}
+# pop_routes_dest_dict = {"LGW": 'London Gatwick Airport, United Kingdom, London',
+#                         "BEG": 'Belgrade Nikola Tesla Airport, Serbia',
+#                         "MAJ": 'Marshall Islands International Airport',
+#                         "SDU": 'Santos Dumont Airport, Brazil', "YKS": 'Yakutsk Airport, Russia',
+#                         "LUX": 'Luxembourg-Findel International Airport, Luxembourg',
+#                         "ERS": 'Eros Airport, Namibia',
+#                         "PPT": 'Faa International Airport, French Polynesia',
+#                         "KTM": 'Tribhuvan International Airport, Nepal',
+#                         "ALA": 'Almaty Airport, Kazakhstan'}
+#
+# pr_1.replace({'ORIGIN': pop_routes_origin_dict}, inplace = True)
+# # , 'DESTINATION': pop_routes_dest_dict},
+# print(pr_1)
