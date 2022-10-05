72e51eeaea28a419e54f958be5b47362e828314f
Aditya Rane <adityasatyaprakash.rane@jcu.edu.au>
2022-10-05 10:07:13 +0800

    Updated charts

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,12 +34,14 @@ def load_lottieurl(url):
 
 # ---- LOAD ASSETS ----
 lottie_emission = load_lottieurl("https://assets1.lottiefiles.com/private_files/lf30_dy2xdidy.json")
+lottie_emission_1 = load_lottieurl("https://assets9.lottiefiles.com/packages/lf20_bknkrt5y.json")
 lottie_burn = load_lottieurl("https://assets6.lottiefiles.com/packages/lf20_ZhtOc0.json")
 lottie_airport = load_lottieurl("https://assets6.lottiefiles.com/packages/lf20_jlmgqgx2.json")
 lottie_worst_fl = load_lottieurl("https://assets6.lottiefiles.com/packages/lf20_vyixuzos.json")
 lottie_avoid = load_lottieurl("https://assets1.lottiefiles.com/packages/lf20_ymjrns2k.json")
 lottie_fuel_burn = load_lottieurl("https://assets8.lottiefiles.com/packages/lf20_no9qrf5p.json")
 lottie_CO2 = load_lottieurl("https://assets1.lottiefiles.com/packages/lf20_wst709y2.json")
+lottie_cust = load_lottieurl("https://assets8.lottiefiles.com/packages/lf20_EHugAD.json")
 # img_contact_form = Image.open("images/yt_contact_form.png")
 # img_lottie_animation = Image.open("images/yt_lottie_animation.png")
 
@@ -62,10 +64,10 @@ with st.container():
         st.image(deloitte_img)
 
 # Defining different tabs
-tab1, tab2, tab3, best_flights_tab, worst_flights_tab, avoid_flights_tab, fuel_burn_tab, CO2_tab = st.tabs([
+tab1, tab2, tab3, best_flights_tab, worst_flights_tab, avoid_flights_tab, fuel_burn_tab, CO2_tab, cust_tab = st.tabs([
     "SkyScanner",
     "Greenhouse Impact", "Busiest Airports for Departures", "Top 5 flights", "Non-Eco-friendly Flights",
-    "Flights to Avoid", "Fuel Burn", 'CO2 Emission'])
+    "Flights to Avoid", "Fuel Burn", 'CO2 Emission', 'Best Flights for Customers'])
 # tab4
 
 with tab1:
@@ -175,8 +177,11 @@ with best_flights_tab:
             best_flights_bar.replace({'AIRLINECODE': best_flights_dict}, inplace=True)
             best_flights_bar = best_flights_bar.set_index(['AIRLINECODE'])
             st.bar_chart(best_flights_bar)
+        with right_column:
+            st_lottie(lottie_emission_1, height=300, key="emission_1")
 
 with worst_flights_tab:
+    # st.header("Popular Routes")
     with st.container():
         st.write("---")
         left_column, right_column = st.columns(2)
@@ -184,12 +189,13 @@ with worst_flights_tab:
             st.subheader("Top 5 Non-Eco-friendly flights")
 
             worst_flights = pd.read_sql(
-                f"SELECT AIRLINECODE,FLIGHTNUMBER ,MAX(ESTIMATED_C02_EFFICIENCY) MAX_ESTIMATED_CO2_EFFICIENCY FROM(SELECT AIRLINECODE,FLIGHTNUMBER,SCHEDULED_DEPARTURE_DATE, ESTIMATED_CO2_EMISSIONS_TONNES/ESTIMATED_FUEL_BURN_TONNES as ESTIMATED_C02_EFFICIENCY,DENSE_RANK() OVER (ORDER BY ESTIMATED_C02_EFFICIENCY DESC)my_dense_rank FROM SCHEDULED_FLIGHT_EMISSIONS WHERE (ESTIMATED_FUEL_BURN_TONNES) <> 0) table1 GROUP BY AIRLINECODE,FLIGHTNUMBER ORDER BY MAX_ESTIMATED_CO2_EFFICIENCY desc LIMIT 5 ",
+                f"SELECT AIRLINECODE,FLIGHTNUMBER, MAX(ESTIMATED_C02_EFFICIENCY) MAX_ESTIMATED_CO2_EFFICIENCY FROM(SELECT AIRLINECODE,FLIGHTNUMBER,SCHEDULED_DEPARTURE_DATE, ESTIMATED_CO2_EMISSIONS_TONNES/ESTIMATED_FUEL_BURN_TONNES as ESTIMATED_C02_EFFICIENCY,DENSE_RANK() OVER (ORDER BY ESTIMATED_C02_EFFICIENCY DESC)my_dense_rank FROM SCHEDULED_FLIGHT_EMISSIONS WHERE (ESTIMATED_FUEL_BURN_TONNES) <> 0) table1 GROUP BY AIRLINECODE,FLIGHTNUMBER ORDER BY MAX_ESTIMATED_CO2_EFFICIENCY desc LIMIT 5 ",
                 conn)
             worst_flights = pd.DataFrame(worst_flights)
-            worst_flights_dict = {"OZ": 'Asiana Airlines Inc.', "NH": 'All Nippon Airways Co., Ltd',
-                                  "GR": 'Aurigny Airways', "UA": 'United Airlines', 'MH': "Malaysia Airlines"}
-            worst_flights.replace({'AIRLINECODE': worst_flights_dict}, inplace=True)
+            worst_flights_dict = {'OZ': 'Asiana Airlines', 'GR': 'Aurigny Airlines', "UA": 'United AIrlines',
+                                  "NH": 'All Nippon Airways Co., Ltd.', "MH": 'Malaysian Airlines Berhad'}
+            worst_flights.replace({'AIRLINECODE': worst_flights_dict}, inplace = True)
+            # worst_flights = worst_flights.set_index(['AIRLINECODE'])
             st.dataframe(worst_flights)
         with right_column:
             st_lottie(lottie_worst_fl, height=300, key="worst_fl")
@@ -249,5 +255,24 @@ with CO2_tab:
             st.bar_chart(CO2_em)
         with right_column:
             st_lottie(lottie_CO2, height=300, key="CO2")
+        st.write(
+            "[Airport Codes>](https://www.world-airport-codes.com/alphabetical/airport-code/a.html?page=1)")
+
+with cust_tab:
+    # st.header("Popular Routes")
+    with st.container():
+        st.write("---")
+        left_column, right_column = st.columns(2)
+        with left_column:
+            st.subheader("Best flights for customers")
+
+            cust = pd.read_sql(
+                f"SELECT AIRLINECODE, MAX(CARBON_OFFSET_FEE_PER_CUSTOMER) as MIN_CARBON_OFFSET_FEE_PER_CUSTOMER FROM(SELECT  AIRLINECODE, FLIGHTNUMBER, ESTIMATED_CO2_EMISSIONS_TONNES, SEATS, ((ESTIMATED_CO2_EMISSIONS_TONNES *10.50) / SEATS) as CARBON_OFFSET_FEE_PER_CUSTOMER FROM SCHEDULED_FLIGHT_EMISSIONS WHERE SEATS <> 0) GROUP BY AIRLINECODE, FLIGHTNUMBER ORDER BY MIN_CARBON_OFFSET_FEE_PER_CUSTOMER ASC;",
+                conn)
+            cust = pd.DataFrame(cust)
+            cust = cust.set_index(['AIRLINECODE'])
+            st.bar_chart(cust)
+        with right_column:
+            st_lottie(lottie_cust, height=300, key="cust")
         st.write(
             "[Airport Codes>](https://www.world-airport-codes.com/alphabetical/airport-code/a.html?page=1)")
